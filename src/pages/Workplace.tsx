cd347d844899cee555f59b89faa8e84c1bd75971
aswin-om <aswin@ombrulla.com>
2025-11-24 00:32:57 +0530

    Add build workflow and update Workplace

--- src/pages/Workplace.tsx
+++ src/pages/Workplace.tsx
@@ -1,6 +1,5 @@
 import { Editor } from 'components/editor'
 import Sidebar from '../components/Sidebar'
-
 export default function Workplace() {
   return (
     <div className="barlow-regular flex h-full">
